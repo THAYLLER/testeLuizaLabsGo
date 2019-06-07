25c92b98b1af6b4c7eb5d02e8ee6ec88f3354b4b
Thayller Vilela Cintra <thayllervilelacintra@yahoo.com.br>
2019-06-06 23:50:33 -0300

    Add files via upload

--- testeLabs/main.go
+++ testeLabs/main.go
@@ -1,33 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"os"
+	"fmt"
+	"io/ioutil"
 )
 
-const CAMINHO = "txt/"
+const CAMINHO string = ""
 
-// Funcao que le o conteudo do arquivo e retorna um slice the string com todas as linhas do arquivo
-func lerTexto(caminhoDoArquivo string) ([]string, error) {
+func lerTxt(nomeArquivo string) string {
 
-	// Abre o arquivo
-	arquivo, err := os.Open(caminhoDoArquivo)
-	// Caso tenha encontrado algum erro ao tentar abrir o arquivo retorne o erro encontrado
-	if err != nil {
-		return nil, err
-	}
-	// Garante que o arquivo sera fechado apos o uso
-	defer arquivo.Close()
+	conteudo, _ := ioutil.ReadFile(CAMINHO + nomeArquivo)
 
-	// Cria um scanner que le cada linha do arquivo
-	var linhas []string
-	scanner := bufio.NewScanner(arquivo)
-	for scanner.Scan() {
-		linhas = append(linhas, scanner.Text())
-	}
-
-	// Retorna as linhas lidas e um erro se ocorrer algum erro no scanner
-	return linhas, scanner.Err()
+	return string(conteudo)
 }
+func main() {
 
-func main() {}
+	fmt.Print(lerTxt("NFe.txt"))
+}
